69563cf67bdc26d8b7bacd3d976e29853a9c8877
Minatulaev Muhammad <minatulaev867@gmail.com>
2022-02-24 22:13:19 +0500

    Добавьте файлы проекта.

---
+++ RomanNumber/RomanNumberException.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace RNE
+{
+    public class RomanNumberException : Exception
+    {
+        public RomanNumberException Vod(int n)
+        {
+            RomanNumberException nol = null;
+            try
+            {
+                
+                if (n == 0 || n < 0)
+                {
+                   throw new Exception("Ошибка: Недопустимое значение, допустимый диапозон {1-1000}");
+                }
+                else
+                {
+                    Console.WriteLine($"Вы ввели: {n}");
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return nol;
+
+        }
+
+
+    }
+
+}
+
